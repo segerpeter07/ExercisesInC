7d64b03687d6530d6871f082f2cfb0ec021aa6eb
Peter Seger <segerpeter07@gmail.com>
2020-01-26 15:47:44 -0500

    Worked through optimization.

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int x = 5;
+    int y = x + 1;
+    printf("Y value: %d\n", y);
     return 0;
 }
+
+
+/* 
+Experiment 1: I think the lines that coresponds to this are:
+                .cfi_def_cfa_register %rbp
+                subq	$16, %rsp
+                movl	$0, -4(%rbp)
+                movl	$5, -8(%rbp)
+	            leaq	L_.str(%rip), %rdi
+
+Experiment 2:  With optimization, all these lines just became:
+                .cfi_def_cfa_register %rbp
+	            leaq	L_str(%rip), %rdi
+
+Experiment 3: When I print X, this line gets added:
+                movl	$5, %esi
+
+Experiment 4: When this addition is added, a new variable doesn't get made, but
+            a variable is just made with it's updated value.
+
+*/
