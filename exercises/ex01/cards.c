e88d2dbf8aaa76f1cda88d7074486a03cd50235f
Peter Seger <segerpeter07@gmail.com>
2020-01-26 15:17:20 -0500

    Completed refactor

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,37 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+
+int det_val(char card_name[]) {
+    /*
+        det_val() determines the value of the card just played.
+
+        card_name: array containing the card "name"
+    */
+    int val = 0;
+    switch(card_name[0]) {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
+        default:
+            val = atoi(card_name);
+            if((val < 1) || (val > 10)) {
+                puts("I don't understand that value!");
+                return 0;
+            }
+    }
+    return val;
+}
+
+int det_count(char card_name[], int count) {
+    /* 
+        det_counter() determines the counter increase/decrease based off the
+        game parameters.
+
+        card_name: array containing the card "name"
+        count: int containing the current game counter value
+    */
+    int val = det_val(card_name);
+    if((val > 2) && (val < 7)) {
+        count++;
+    } else if(val == 10) {
+        count--;
+    }
+    return (count);
+}
+
+void card_counter() {
+    /*
+        card_counter() is the main function that handles getting user input,
+        checking for quit input, and keeping track and printing the count.
+    */
     char card_name[3];
-    int count = 0;
-    while(card_name != 'X') {
+    int start_count = 0;
+    int updated_count = start_count;
+    while (card_name[0] != 'X')
+    {
         puts("Enter the card_name: ");
         scanf("%2s", card_name);
-        int val = 0;
-        switch(card_name[0]) {
-            case 'K':
-            case 'Q':
-            case 'J':
-                val = 10;
-                break;
-            case 'A':
-                val = 11;
-                break;
-            case 'X':
-                continue;
-            default:
-                val = atoi(card_name);
-                if((val < 1) || (val > 10)) {
-                    puts("I don't understand that value!");
-                    continue;
-                }
-            if((val > 2) && (val < 7)) {
-                count++;
-            } else if(val == 10) {
-                count--;
-            }
-            printf("Current count: %i\n", count);
+        if(card_name[0] == 'X') {
+            break;
         }
-        return 0;
-    }
+        updated_count = det_count(card_name, updated_count);
+        printf("Current count: %d\n", updated_count);
+    }   
+}
+
+
+int main() {
+    card_counter();
+    return 0;
 }
